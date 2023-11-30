2f96d02100caa3f1d5821f6f5da485d22b7797cf
rakha <virgakumara@gmail.com>
2023-11-29 23:24:52 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,36 @@
+function kirimData() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+
+
+    const user = ("virgarakha");
+    const pass = 777;
+    
+    
+
+    if (username == "" ||  password == ""){
+        alert("harap isi username dan pasword")
+    }else if (password != pass ) {
+ 
+    }else if (username != user ) {
+ 
+    }else {
+        alert("Login Berhasil" , document.getElementById("abut").click());
+    }
+
+    const user2 = "Giovani";
+    const pass2 = 438; 
+    
+    if (username == "" ||  password == ""){
+        alert("harap isi username dan pasword")
+    }else if (password != pass2 ) {
+
+    }else if (username != user2 ) {
+
+    }else {
+        alert("Login Berhasil" , document.getElementById("abut").click());
+
+    }   
+    
+     
+}
